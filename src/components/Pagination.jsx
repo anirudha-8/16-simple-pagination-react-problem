f545b6a9d18bc8d1aac82ad3acbc4f88f4c3dc95
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-11 07:30:40 +0530

    calculate index of first and last item, to find out current items in page

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 
 const Pagination = ({ items, itemsPerPage }) => {
-    
+
     const [currentPage, setCurrentPage] = useState(1);
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+    const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
     
 	return ();
 };

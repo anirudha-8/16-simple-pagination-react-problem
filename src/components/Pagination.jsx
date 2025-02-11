99fccab5ae2893040a5fcc65dea17aef855a4e80
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-11 07:26:44 +0530

    add state for the current page

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,4 +1,9 @@
-const Pagination = () => {
-	return <div>Pagination</div>;
+import { useState } from "react";
+
+const Pagination = ({ items, itemsPerPage }) => {
+    
+    const [currentPage, setCurrentPage] = useState(1);
+    
+	return ();
 };
 export default Pagination;
